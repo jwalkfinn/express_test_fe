a390c74a9b7bc6544d6f82674e0b39eb1a996af9
Justin Walk <justinwalk@Justins-MacBook-Pro.local>
2020-10-14 11:40:48 -0700

    add loop for user

--- app.js
+++ app.js
@@ -2,14 +2,61 @@ const express = require('express')
 const app = express()
 const port = 3100
 
+var rp = require('request-promise');
+
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+async function createUser() {
+  var id = "";
+  var response = {};
+
+  await rp('http://localhost:3000/id').then(function(idResponse) {
+    id = JSON.parse(idResponse)["id"]
+    var options = {
+      method: 'POST',
+      uri: 'http://localhost:3000/user',
+      body: {
+        id: id,
+        firstName: "Sample",
+        lastName: "McTestington",
+        email: "sample@email.com"
+      },
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      json: true
+    };
+    return rp(options)
+  }).then(function(createResponse) {
+    return rp(`http://localhost:3000/user?id=${id}`)
+  }).then(function(getResponse) {
+    response = JSON.parse(getResponse)
+    return response
+  });
+  return response
+};
+
+app.get('/', async (req, res, next) => {
+  var counter = 1;
+  var needUser = true;
+  var user = {};
+
+  while (needUser) {
+    try {
+      var userResponse = await createUser();
+      user = userResponse;
+      needUser = false;
+    } catch (error) {
+      counter += 1;
+    };
+  }
+
   res.render('index', {
-    id: 'testID',
-    firstName: "Testley",
-    lastName: "Testington",
-    email: "test@email.com"
+    id: user["id"],
+    firstName: user["firstName"],
+    lastName: user["lastName"],
+    email: user["email"],
+    counter: counter
   })
 })
 
